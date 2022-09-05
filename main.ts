f3e60d803573d2476bb0337ad51307db526ca324
yene27 <yene27@yahoo.com>
2022-09-05 10:15:43 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -42,9 +42,9 @@ input.onButtonPressed(Button.B, function () {
     cabezaDino.change(LedSpriteProperty.Y, -1)
     cabezaDino.change(LedSpriteProperty.X, -1)
 })
+let tocoObstaculo = 0
 let cuerpoDino: game.LedSprite = null
 let cabezaDino: game.LedSprite = null
-introJuego()
 cabezaDino = game.createSprite(1, 3)
 cuerpoDino = game.createSprite(1, 4)
 let obstaculo = game.createSprite(4, randint(3, 4))
@@ -52,15 +52,20 @@ led.setBrightness(103)
 game.setScore(0)
 game.setLife(2)
 basic.forever(function () {
+    tocoObstaculo = 0
     basic.pause(500)
     obstaculo.change(LedSpriteProperty.X, -1)
     if (obstaculo.isTouching(cuerpoDino) || obstaculo.isTouching(cabezaDino)) {
+        tocoObstaculo = 1
+        basic.pause(1000)
         game.removeLife(1)
     }
     if (obstaculo.get(LedSpriteProperty.X) == 0) {
         basic.pause(500)
         obstaculo.set(LedSpriteProperty.X, 4)
         obstaculo.set(LedSpriteProperty.Y, randint(3, 4))
-        game.setScore(game.score() + 1)
+        if (tocoObstaculo == 0) {
+            game.setScore(game.score() + 1)
+        }
     }
 })
